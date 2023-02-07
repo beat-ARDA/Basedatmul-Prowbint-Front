575ef516dea81946a97b2964e98ae1bc2f0579a7
Alvaro Duron <alvaro_07051@outlook.com>
2023-02-07 11:48:20 -0600

    acomplamiento pagina registro con navbar

--- src/components/registro/registro.jsx
+++ src/components/registro/registro.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './registro.css';
 
 export default function Registro() {
     return (
         <div className='container-fluid d-flex justify-content-center align-items-center'>
-            <form className='w-50'>
+            <form className='w-50 padre-registro'>
                 <div className='row m-0 pb-1'>
                     <div className='col-12 m-0 p-0 d-flex justify-content-center align-items-center'>
                         <h4 className='fw-bold'>Empieza hoy con tu cuenta de cursos!</h4>
@@ -13,7 +14,7 @@ export default function Registro() {
                     <div className='col-12 m-0 p-0 d-flex justify-content-center align-items-center'>
                         <input aria-describedby="reglas-nombres" name="nombres" className='form-control' type={"text"} placeholder="Nombres..." />
                     </div>
-                    <div id="reglas-nombres" class="form-text">
+                    <div id="reglas-nombres" className="form-text">
                         Este campo es (Obligatorio)
                     </div>
                 </div>
@@ -21,7 +22,7 @@ export default function Registro() {
                     <div className='col-12 m-0 p-0 d-flex justify-content-center align-items-center'>
                         <input aria-describedby="reglas-apellidos" name="apellidos" className='form-control' type={"text"} placeholder="Apellidos..." />
                     </div>
-                    <div id="reglas-apellidos" class="form-text">
+                    <div id="reglas-apellidos" className="form-text">
                         Este campo es (Obligatorio)
                     </div>
                 </div>
@@ -50,14 +51,14 @@ export default function Registro() {
                     <div className='col-12 m-0 p-0 d-flex justify-content-center align-items-center'>
                         <input aria-describedby="reglas-contraseña" placeholder='Contraseña...' className="form-control" type="password" id="contraseña" name="contraseña" />
                     </div>
-                    <div id="reglas-contraseña" class="form-text">
+                    <div id="reglas-contraseña" className="form-text">
                         8 caracteres al menos, una mayuscula, un caracter especial y un numero al menos.
                     </div>
                 </div>
                 <div className='row m-0 pb-1'>
                     <div className='col-12 m-0 p-0 d-flex justify-content-center align-items-center'>
-                        <select aria-describedby="reglas-tipo-usuario" className="form-select text-secondary" aria-label="Tipo de usuario" name="tipo-usuario">
-                            <option selected>Selecciona el tipo de usuario</option>
+                        <select defaultValue={"none"} aria-describedby="reglas-tipo-usuario" className="form-select text-secondary" aria-label="Tipo de usuario" name="tipo-usuario">
+                            <option defaultValue={"none"}>Selecciona el tipo de usuario</option>
                             <option value="Alumno">Alumno</option>
                             <option value="Instructor">Instructor</option>
                         </select>
@@ -68,8 +69,8 @@ export default function Registro() {
                 </div>
                 <div className='row m-0 pb-1'>
                     <div className='col-12 m-0 p-0 d-flex justify-content-center align-items-center'>
-                        <select aria-describedby="reglas-genero" className="form-select text-secondary" aria-label="Tipo de usuario" name="tipo-usuario">
-                            <option selected>Selecciona un genero</option>
+                        <select defaultValue={"none"} aria-describedby="reglas-genero" className="form-select text-secondary" aria-label="Tipo de usuario" name="tipo-usuario">
+                            <option defaultValue={"none"}>Selecciona un genero</option>
                             <option value="Masculino">Masculino</option>
                             <option value="femenino">Femenino</option>
                         </select>
@@ -80,7 +81,7 @@ export default function Registro() {
                 </div>
                 <div className='row m-0 p-0'>
                     <div className='col-12 m-0 p-0 d-flex justify-content-center align-items-center'>
-                        <button type="submit" class="btn btn-dark w-100">Registrarse</button>
+                        <button type="submit" className="btn btn-dark w-100">Registrarse</button>
                     </div>
                 </div>
             </form>
