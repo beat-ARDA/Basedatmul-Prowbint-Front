adb218c789f4669cbc100e8eeba8918bd764ed1c
ZaredMaldon <zaredosvaldom@gmail.com>
2023-05-26 21:59:38 -0600

    cambios

--- src/servicesPw2/courses.js
+++ src/servicesPw2/courses.js
@@ -1,3 +1,22 @@
+async function getCoursesActive() {
+    return await fetch(process.env.REACT_APP_PATH_API + `curso/mostrar/active`, {
+        method: 'GET',
+        dataType: "json"
+    })
+        .then(response => response.text())
+        .then(data => { 
+            return JSON.parse(data) })
+        .catch(error => console.warn(error));
+};
 
+async function getCourseById(idCurso) {
+    return await fetch(process.env.REACT_APP_PATH_API + `curso/${idCurso}`, {
+        method: 'GET',
+        dataType: "json"
+    })
+        .then(response => response.text())
+        .then(data => { return JSON.parse(data) })
+        .catch(error => console.warn(error));
+};
 
-export{};
+export{getCoursesActive,getCourseById};
